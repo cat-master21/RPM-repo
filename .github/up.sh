bf2a508ade11620f6d34b83597fce88ad26ea432
cat-master21 <96554164+cat-master21@users.noreply.github.com>
2022-08-05 09:59:46 -0700

    debug

--- .github/up.sh
+++ .github/up.sh
@@ -2,6 +2,11 @@
 
 if [[ "$(git log -n 1 --pretty=format:%H)" == "$(git log -n 1 --pretty=format:%H -- *.rpm)" ]]; then
             echo yes
+            git log -n 1 --pretty=format:%H
+            echo =========
+            git log -n 1 --pretty=format:%H -- *.rpm
+            echo =========
+            git log -n 1
 else
 echo no
 fi
